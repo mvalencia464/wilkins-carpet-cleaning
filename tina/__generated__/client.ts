f918b8809d313ef2ceba15751af091e6adb5054c
mvalencia464 <mauricio@stokeleads.com>
2025-09-19 08:03:03 -0700

    Update generated client and Bolt config

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/mauriciovalencia/Desktop/tina/tina/__generated__/.cache/1758292971200', url: 'https://content.tinajs.io/1.6/content/7f0541cf-76a8-4193-ab54-da65437b3641/github/main', token: '1ae312568159920fafbdd2c2f725fc719768b103', queries,  });
+export const client = createClient({ cacheDir: '/Users/mauriciovalencia/Desktop/tina/tina/__generated__/.cache/1758293654033', url: 'https://content.tinajs.io/1.6/content/7f0541cf-76a8-4193-ab54-da65437b3641/github/main', token: '1ae312568159920fafbdd2c2f725fc719768b103', queries,  });
 export default client;
   
