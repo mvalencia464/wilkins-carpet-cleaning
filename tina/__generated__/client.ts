76cb7c64edb8c3d526d42a335775636d5e5d49f2
mvalencia464 <75393762+mvalencia464@users.noreply.github.com>
2025-09-19 09:12:55 -0700

    Updated client.ts

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '1ae312568159920fafbdd2c2f725fc719768b103', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a1b2c3d4e5f6a7b8c9', queries,  });
 export default client;
   
