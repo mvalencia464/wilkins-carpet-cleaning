b2438f002ed4abb5f057211117f9c2b54e4fd734
mvalencia464 <75393762+mvalencia464@users.noreply.github.com>
2025-09-19 07:10:22 -0700

    Updated client.ts

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'undefined', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: '1ae312568159920fafbdd2c2f725fc719768b103', queries,  });
 export default client;
   
